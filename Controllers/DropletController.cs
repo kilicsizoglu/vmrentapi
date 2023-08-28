cca243efd2f13876578e0f8b0e345aaae17d1a01
Fatih YOLCU <128999435+kilicsizoglu@users.noreply.github.com>
2023-08-28 15:39:28 +0300

    Add project files.

---
+++ Controllers/DropletController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+using vmrentapi.Data;
+
+namespace vmrentapi.Controllers
+{
+    public class DropletController : Controller
+    {
+        DropletManager dropletManager;
+        public DropletController() 
+        {
+            dropletManager = new DropletManager();
+        }
+
+        public async Task<IActionResult> GetAllDropletAsync()
+        {
+            var droplets = await dropletManager.GetAllDropletAsync();
+            return Ok(droplets);
+        }
+
+        public async Task<IActionResult> GetDropletAsync([FromBody] long id)
+        {
+            var droplet = await dropletManager.GetDropletAsync(id) ;
+            return Ok(droplet);
+        }
+
+        public IActionResult CreateDroplet([FromBody] string name, [FromBody] string size, [FromBody] string image)
+        {
+            dropletManager.CreateDroplet(name, size, image);
+            return Ok("ok");
+        }
+
+        public IActionResult DeleteDroplet([FromBody] long id)
+        {
+            dropletManager.DeleteDroplet(id);
+            return Ok("ok");
+        }
+    }
+}
---
+++ Data/DropletManager.cs
@@ -0,0 +1,46 @@
+﻿using DigitalOcean.API;
+using DigitalOcean.API.Models.Requests;
+
+namespace vmrentapi.Data
+{
+    
+
+    public class DropletManager
+    {
+
+        public DigitalOceanClient client;
+
+        public DropletManager()
+        {
+            client = new DigitalOceanClient("");
+        }
+
+        public void CreateDroplet(String Name, String Size, String Image)
+        {
+            var request = new DigitalOcean.API.Models.Requests.Droplet
+            {
+                Name = Name,
+                Size = Size,
+                Image = Image
+            };
+
+            client.Droplets.Create(request);
+        }
+
+        public void DeleteDroplet(long id)
+        {
+            client.Droplets.Delete(id);
+        }
+
+        public async Task<DigitalOcean.API.Models.Responses.Droplet> GetDropletAsync(long id)
+        {
+            return await client.Droplets.Get(id);
+        }
+
+        public async Task<IReadOnlyList<DigitalOcean.API.Models.Responses.Droplet>> GetAllDropletAsync()
+        {
+            return await client.Droplets.GetAll();
+        }
+
+    }
+}
